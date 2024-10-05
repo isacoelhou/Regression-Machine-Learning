1a4fb59d56a7a12c6ed721171fb1e4cac3ed424c
isacoelhou <isadora.araujo1@unioeste.br>
2024-10-05 17:00:37 -0300

    knr

--- Regressors/Regressors.py
+++ Regressors/Regressors.py
@@ -3,22 +3,56 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
 
-dados = pd.read_excel("../Dataset/ResidencialBuilding.xlsx")
+def grid_search_KNR():
 
-dados = shuffle(dados)
+    maior = 10000000000
+    for j in ("distance","uniform"):
+        for i in range (1,50):
 
-X = dados.iloc[:, :-2]  
-Y = dados.iloc[:, -2:]  # Outputs
+            KNN = KNeighborsRegressor(n_neighbors=i,weights=j)
+            KNN.fit(x_treino, y_treino)
 
-print(dados.info())
-print(dados.describe()) 
+            opiniao = KNN.predict(x_validacao)
 
-# plt.figure(figsize=(10, 8))
-# sns.heatmap(dados.corr(), annot=True, cmap='coolwarm')
-# plt.title('Mapa de Calor da Correlação entre os Atributos')
-# plt.show()
+            mae = mean_absolute_error(y_validacao, opiniao)
+            mse = mean_squared_error(y_validacao, opiniao)
+            rmse = np.sqrt(mse)
 
-x_treino, x_temp, y_treino, y_temp = train_test_split(X, Y, test_size=0.5)
-x_validacao, x_teste, y_validacao, y_teste = train_test_split(x_temp, y_temp, test_size=0.5)
+            media = (mae + mse + rmse)/3
 
+            if(media < maior):
+                best_n = i
+                best_w = j
+
+    return best_n, best_w
+
+
+rmse_KNN = []
+mse_KNN = []
+mae_KNN = []
+
+for _ in range(20):
+
+    dados = pd.read_excel("../Dataset/ResidencialBuilding.xlsx")
+
+    dados = shuffle(dados)
+
+    X = dados.iloc[:, :-2]  
+    Y = dados.iloc[:, 2:]  # Outputs
+
+    x_treino, x_temp, y_treino, y_temp = train_test_split(X, Y, test_size=0.5)
+    x_validacao, x_teste, y_validacao, y_teste = train_test_split(x_temp, y_temp, test_size=0.5)
+
+    i, j = grid_search_KNR()
+    KNN = KNeighborsRegressor(n_neighbors=i,weights=j)
+    KNN.fit(x_treino,y_treino)
+
+    opiniao = KNN.predict(x_teste)
+
+    mae_KNN.append(mean_absolute_error(y_validacao, opiniao))
+    mse_KNN.append(mean_squared_error(y_validacao, opiniao))
+    rmse_KNN.append(np.sqrt(mean_squared_error(y_validacao, opiniao)))
