b00a49298910314ab08178eb3ce7811304e104b7
Leo-Calsavara <calsavara2511@gmail.com>
2024-10-08 17:12:29 -0300

    Heatmap ajustado

--- Regressors/Heatmap.py
+++ Regressors/Heatmap.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Carregar a base de dados
-df = pd.read_excel("../Dataset/data.xlsx")
+df_origin = pd.read_excel("../Dataset/data.xlsx")
+
+df_red = df_origin.iloc[:,:-77]
+df = pd.concat([df_red, df_origin['V-9']], axis=1)
 
 
 # Informações da base de dados
@@ -14,18 +17,6 @@ valores_medios = df.mean()
 valores_maximos = df.max()
 valores_minimos = df.min()
 correlacao = df.corr()
-
-# Imprimir informações
-print(f"Tamanho da base de dados: {tamanho} linhas")
-print(f"Dimensão da base de dados: {dimensao} colunas")
-print("\nTipos dos atributos:")
-print(tipos_atributos)
-print("\nValores Médios de cada coluna:")
-print(valores_medios)
-print("\nValores Máximos de cada coluna:")
-print(valores_maximos)
-print("\nValores Mínimos de cada coluna:")
-print(valores_minimos)
 
 # Plotar a matriz de correlação
 plt.figure(figsize=(20, 16))  # Ajuste o tamanho da figura conforme necessário
@@ -38,4 +29,3 @@ plt.ylabel('Variáveis', fontsize=18)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 plt.show()
-
